f0d8008d41381c6c02268c205dad35dd33cf3319
Jyoti Singh <jyotisingh56968@gmail.com>
2026-03-08 17:30:34 +0530

    final render fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// students data
+// Students data
 let students = [
   { id: 101, name: "Rahul Sharma", course: "BSc CS" },
   { id: 102, name: "Priya Singh", course: "BCA" }
@@ -18,15 +18,20 @@ app.get("/students", (req, res) => {
   res.json(students);
 });
 
-// serve frontend files
-app.use(express.static(__dirname));
+// Serve static files
+app.use(express.static(path.join(__dirname)));
 
-// homepage
+// Home page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+// Catch all route
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
