bd8d5aad94527fc573a5039f13f8869e1183e030
Jyoti Singh <jyotisingh56968@gmail.com>
2026-03-08 12:49:31 +0530

    fix render port

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ students.push(student)
 res.json({message:"Student added successfully"})
 })
 
-app.listen(5000,()=>{
-console.log("Server running on port 5000")
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
